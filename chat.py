52cb641ce6da1c419676d1a72b70dab784e8d0ce
kaueh-silva <kaue.11@hotmail.com>
2024-07-24 19:15:37 -0300

    relizado funca de entrar no chat, apos identificao com nome do usuario

--- chat.py
+++ chat.py
@@ -2,18 +2,28 @@ import flet as ft
 
 
 def main(pagina):
+    pagina.bgcolor = ft.colors.LIGHT_BLUE_ACCENT_400
     titulo = ft.Text("Chat ao vivo")
 
     titulo_janela = ft.Text("Bem vindo ao chat")
     campo_nome_usuraio = ft.TextField(label="Escreve seu nome")
-    botao_entrar = ft.ElevatedButton("Entar no chat")
+
+    def entrar_chat(evento):
+        pagina.remove(titulo)
+        pagina.remove(botao_iniciar)
+        janela.open = False
+        texto_mensagem = ft.TextField(label="Escreva sua mensagem")
+        pagina.add(texto_mensagem)
+        botao_enviar = ft.ElevatedButton("Enviar")
+        pagina.add(botao_enviar)
+        pagina.update
+
+    botao_entrar = ft.ElevatedButton("Entrar no chat", on_click=entrar_chat)
 
     janela = ft.AlertDialog(
-        tittle=titulo_janela,
+        title=titulo_janela,
         content=campo_nome_usuraio,
-        actions=[botao_entrar]
-
-    )
+        actions=[botao_entrar])
 
     def abrir_popup(evento):
         pagina.dialog = janela
